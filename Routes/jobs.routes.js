0abd567877cea02d7cbc243f56d07517740afac9
clerverrepo <abdullahifaruk445@gmail.com>
2025-04-25 20:41:43 +0300

    updated

--- Routes/jobs.routes.js
+++ Routes/jobs.routes.js
@@ -9,14 +9,14 @@ import {
   filterJobs,
   applyJob,
   getApplicationsByUserId,
-  UpdateJobById,
+  updateJobById,
   verifyToken,
 } from "../Controllers/job.controller.js";
 const routes = express.Router();
 routes.post("/", postJob);
 routes.post("/:id/apply", verifyToken, applyJob);
 routes.put("/:id", updateJobById);
-routes.put("/:id/status", UpdateJobById);
+routes.put("/:id/status", updateJobById);
 routes.get("/:id/applications", getApplicationsByUserId);
 routes.get("/filter", filterJobs);
 routes.get("/", getAllJob);
